83cb596f8771fcb2bc6d83bd88c242a25283abf3
katherinecatalano <84155740+katherinecatalano@users.noreply.github.com>
2022-08-24 20:37:00 -0300

    pantalla 1

--- src/UI/components/pantallainicio.js
+++ src/UI/components/pantallainicio.js
@@ -5,13 +5,7 @@ export default function pantallainicio (){
     <Fragment>
         <form>
             <label>
-                const data = [
-                    {_id: 1, label: 'item 1'{"}"},
-                    {_id: 2, label: 'item 2'{"}"},
-                    {_id: 3, label: 'item 3'{"}"},
-
-                ]
-
+                
             </label>
             
 
