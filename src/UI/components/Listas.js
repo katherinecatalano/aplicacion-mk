5224a2824074724dc379ccc39d9275a2f8e6d2af
katherinecatalano <84155740+katherinecatalano@users.noreply.github.com>
2022-08-24 23:45:11 -0300

    cambios

---
+++ src/UI/components/Listas.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+
+
+export default function Listas (){
+    return(
+    <Fragment>
+        const options = [
+            'one', 'two', 'three'
+        ];
+
+
+        const defaultOption = option[0];
+        <Dropdown options= { options } onChange={this._onSelect} value= {defaultOption} placeholder="Seleccione una opcion" />
+        </Fragment>
+    )
+}
+    
