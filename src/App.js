488ffc8c47d4ef537fca7be20f2d95d87c640bac
katherinecatalano <84155740+katherinecatalano@users.noreply.github.com>
2022-11-09 00:18:25 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React, { } from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import Registro from './UI/components/Registro';
 import Login from './UI/components/Login'; 
 import Pantalla1 from './UI/components/Pantalla1';
 import Incidencia from './UI/components/Incidencia';
-// import Listas from './UI/components/Listas';
+/*import Listas from './UI/components/Listas';*/
 import Registroincidencia from './UI/components/Registroincidencia';
+import { Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 export default function App() {
   return (
-
-    <view>
-       <Router>
-        <div>
-          <Routes>
-            <Route exact path="/Login" element={<Registro />}/>
-          </Routes>
-        </div>
-      </Router>
-      
+    <div>
+      {Router}
+    <Fragment>
+    
+      <view>
       <Registro></Registro>
       <Login></Login>
       <Pantalla1></Pantalla1>
       <Incidencia></Incidencia>
-      <comentarios></comentarios>
-      {/* <Listas></Listas> */}
-      <Registroincidencia></Registroincidencia> 
-
+        {/* <Listas></Listas> */}
+      <Registroincidencia></Registroincidencia>
     </view>
+    </Fragment>
+    {Router}
+    </div>
+
 
   )
 }
