29ab84ae587bca25ccf85cadf8646e520d2f33b3
katherinecatalano <84155740+katherinecatalano@users.noreply.github.com>
2022-11-15 16:29:37 -0300

    boton

---
+++ src/UI/components/Boton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import{} from React;
+
+const Boton = () => {
+    return (
+        <view>
+            <text>Boton</text>
+        </view>
+    )
+}
+
+export default Boton
