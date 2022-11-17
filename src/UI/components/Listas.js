d26f36ca70d70a35e1872a13e040a5d85661b935
katherinecatalano <84591810+melisa-campos@users.noreply.github.com>
2022-11-17 15:58:18 -0300

    Update Listas.js

--- src/UI/components/Listas.js
+++ src/UI/components/Listas.js
@@ -1,38 +1,8 @@
-/*
- import React, {}from "react";
+import firebase, { db } from 'FirabaseConfig';
+import { collection, getDocs, query, doc, addDoc, deleteDoc, updateDoc } from 'FirebaseConfig';
 
-    class Hello extends React.Component {
-        render() {
-            return <div className="nav">
-                <Link />
-                <Link />
-                <Link />
-            </div>;
-    }
-  }
-  
-  class Link extends React.Component {
-      state = {
-        open: false
-    }
-    handleClick = () => {
-        this.setState({ open: !this.state.open });
-    }
-    render () {
-        const { open } = this.state;
-        return (
-          <div className="link">
-            <span onClick={this.handleClick}>Click Me</span>
-          <div className={`menu ${open ? 'open' : ''}`}>
-            <ul>
-              <li>Test 1</li>
-              <li>Test 2</li>
-              <li>Test 3</li>
-            </ul>
-          </div>
-          </div>
-      )
-    }
-  }
 
-*/
+export const savePersonName = (name) => {
+    addDoc(collection(db, 'persons'), { name });
+
+}
