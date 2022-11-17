980de1f4e962f8c231941b1b4b7b6fc9a7ea400d
katherinecatalano <84155740+katherinecatalano@users.noreply.github.com>
2022-11-17 15:53:35 -0300

    lista

--- src/UI/components/Listas.js
+++ src/UI/components/Listas.js
@@ -2,7 +2,7 @@ import firebase, { db } from 'FirabaseConfig';
 import { collection, getDocs, query, doc, addDoc, deleteDoc, updateDoc } from 'FirebaseConfig';
 
 
-const savePersonName = (name) => {
+export const savePersonName = (name) => {
     addDoc(collection(db, 'persons'), { name });
 
 }
