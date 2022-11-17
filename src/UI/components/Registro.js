fcbac50837881dcaee08d99971dea7facb680a8a
katherinecatalano <84591810+melisa-campos@users.noreply.github.com>
2022-11-17 13:51:47 -0300

    Update Registro.js

--- src/UI/components/Registro.js
+++ src/UI/components/Registro.js
@@ -1,24 +1,24 @@
-import React, { Fragment } from "react";
+import React,  from "react";
 import Input from "./Input";
 /*import { BrowseRouter as Router, Routes, Route,Link} from 'react-router-dom';*/
 
-
-export default function Registro (){
+import './app.css';
+export default function App (){
     return (
-        <Fragment>
+        <div>
                 <h1>MK FIX</h1>
                 <h3>Registro</h3>
                 <p>Complete los siguientes datos</p>
                 
-                <div>
+                <div className="">
                     <Input nombre="Nombre Completo"  place="Nombre"></Input>
-                </div>
-                <div>
-              
+                    <Input id "nombre completo" name= "nombre" value ""</Input>
+                
+        </div>
 
-                    <Input nombre="Numero de DNI"  place="numero"></Input>
+                    nombre="Numero de DNI"  place="numero">
 
-                </div>
+                
                 <div>
                     {/* <label htmlFor="correo">E-mail</label>
                     <br/>
@@ -27,14 +27,18 @@ export default function Registro (){
                     
 
                 </div>
+
                 <div>
+                
                      <label htmlFor="contraseña">Contraseña</label>
                     <br/>
                     <input type="pasword" id="contraseña" name="contraseña" placeholder=""></input> 
                     <Input nombre="Ingrese Contraseña" place="***"></Input>
-                </div>
+                
                 <link className="registrar-login"  to="/Login">Login</link>
                  <button type= "submit">Iniciar Sesion</button>
-        </Fragment>
+
+                 </div>
+        
     )
 }
