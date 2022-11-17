06c404e0e2d3d59b3a94c05df6e305950b4c2191
katherinecatalano <84591810+melisa-campos@users.noreply.github.com>
2022-11-17 15:58:08 -0300

    Update Registroincidencia.js

--- src/UI/components/Registroincidencia.js
+++ src/UI/components/Registroincidencia.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { useState } from 'react';
+import { savePersonName } from 'Listas';
 
-export default function Registroincidencia() {
+function Registroincidencia() {
+
+    const [personName, setPersonName] = useState(initialState null);
+
+    const savePerson = () => {
+        savePersonName(personName);
+    }
 
     return ( <
+<<<<<<< HEAD
     <h1> MK FIX </h1> <br/>
         
         <p> Registsro de incidencia </p>
@@ -13,3 +22,16 @@ export default function Registroincidencia() {
 </ol>
 
 }
+=======
+        div classname = "incidencia" >
+        <
+        input type = "text"
+        onChange = { e => setPersonName(e.target.value) }
+        /> <button onclick = {}>Salvar</button >
+        <
+        /div >
+    );
+}
+
+export default Registroincidencia;
+>>>>>>> 980de1f4e962f8c231941b1b4b7b6fc9a7ea400d
