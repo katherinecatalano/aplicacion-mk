f3dfe7740792340405cc17abdcd0397b716e4527
katherinecatalano <84591810+melisa-campos@users.noreply.github.com>
2022-11-17 15:55:52 -0300

    Update Registroincidencia.js

--- src/UI/components/Registroincidencia.js
+++ src/UI/components/Registroincidencia.js
@@ -3,10 +3,13 @@ import React from "react";
 export default function Registroincidencia() {
 
     return ( <
-        > < h1 > MK FIX < /h1><br / >
-        <
-        p > Registro de incidencia < /p></ >
+    <h1> MK FIX </h1> <br/>
+        
+        <p> Registsro de incidencia </p>
     )
-
+<ol> listas
+    <li>categoria</li>
+    <li></li>
+</ol>
 
 }
