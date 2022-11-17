fade9dfcfdb74f8da86107ef30465d7d68101d46
katherinecatalano <84155740+katherinecatalano@users.noreply.github.com>
2022-11-17 15:47:10 -0300

    estados

--- src/UI/components/Registroincidencia.js
+++ src/UI/components/Registroincidencia.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { useState } from 'react';
+import { savePersonName } from 'Listas';
 
 function Registroincidencia() {
 
     const [personName, setPersonName] = useState(initialState null);
+
+    const savePerson = () => {
+        savePersonName(personName);
+    }
 
     return ( <
         div classname = "incidencia" >
