f540375c7c6668e706b592a3fcf77988974d66f0
katherinecatalano <84155740+katherinecatalano@users.noreply.github.com>
2022-11-20 17:26:14 -0300

    firebase

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -6,17 +6,18 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD4BzkklMLZiypxKKlEleyx8y6zaRSzzjY",
-  authDomain: "mkfixapii.firebaseapp.com",
-  projectId: "mkfixapii",
-  storageBucket: "mkfixapii.appspot.com",
-  messagingSenderId: "619235234137",
-  appId: "1:619235234137:web:8de2bab9f097ce0d2f7c48",
-  measurementId: "G-5RPDYPDH8P"
+    apiKey: "AIzaSyD4BzkklMLZiypxKKlEleyx8y6zaRSzzjY",
+    authDomain: "mkfixapii.firebaseapp.com",
+    databaseURL: "https://mkfixapii-default-rtdb.firebaseio.com",
+    projectId: "mkfixapii",
+    storageBucket: "mkfixapii.appspot.com",
+    messagingSenderId: "619235234137",
+    appId: "1:619235234137:web:c5d965f1d1098b042f7c48",
+    measurementId: "G-ES8MTWFQBR"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db= getFirestore(app) ;
 const analytics = getAnalytics(app);
