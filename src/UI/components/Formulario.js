44363b3e5d5232feb04f71939e48c3df6638f0e1
katherinecatalano <84591810+melisa-campos@users.noreply.github.com>
2022-11-20 15:23:48 -0300

    Update Formulario.js

--- src/UI/components/Formulario.js
+++ src/UI/components/Formulario.js
@@ -5,13 +5,45 @@ import {useForm}  from "react-hook-form"
 const Formulario = () => {
 
 
-    const { register }  = useForm ();
+    const { register, handleSubmit }  = useForm ();
 
 
 
     return <div>
         <h2> Formulario</h2>
         <form>
+            <div> 
+                <label>Nombre</label>
+                <input type= "text" name= ""/>
+            </div>
+
+            <div> 
+                <label>Direccion</label>
+                <input type= "text" name= ""/>
+            </div>
+
+
+            <div> 
+                <label>Edad</label>
+                <input type= "text" name= ""/>
+            </div>
+
+
+            <div> 
+                <label>Pais</label>
+                <select>
+                    <option value="es">España</option>
+                    <option value="it">italia</option>
+                    <option value="">Argentina</option>
+                    
+                
+
+            
+                </select>
+            </div>
+
+
+
 
         </form>
 
