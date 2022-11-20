d750d9d95aaff234bb8fe8eab8c0c8ff56a85d7e
katherinecatalano <84155740+katherinecatalano@users.noreply.github.com>
2022-11-20 18:27:32 -0300

    inicio

--- src/UI/components/Inicio.js
+++ src/UI/components/Inicio.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import { FirebaseApp } from "firebase/app";
+import { fireEvent, render } from "@testing-library/react";
 
 
-function pantallainicio() {
-  return (
-    <div>pantallainicio</div>
-  )
+export default class Inicio extends Component {
+
+    logout() {
+        /*cierro sesion*/
+        firebaseConfig.auth().signOut();
+    }
+    render() {
+        return ( <
+            div >
+            <
+            h1 > Pantalla inicio, crear incidencia < /h1> <
+            button onClick = { this.logout } > Cerrar sesion < /button> < /
+            div >
+        );
+    }
+
 }
-
-export default pantallainicio;
-
