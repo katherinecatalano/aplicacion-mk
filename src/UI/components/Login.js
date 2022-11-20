b817c36e2e32f0a58373466c1ce8f570b34cfed3
katherinecatalano <84155740+katherinecatalano@users.noreply.github.com>
2022-11-20 17:26:39 -0300

    login

--- src/UI/components/Login.js
+++ src/UI/components/Login.js
@@ -1,1 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
+import firebase from '/firebase';
+
+
+export default class Login extends Component {
+    usuario: React.createRef();
+    contraseña: React.createRef();
+
+    constructor(props) {
+        super(props);
+        //el enlace es para que this funcione y vuleva el dato*/
+
+        this.login = this.login.bind(this);
+        this.signup = thi.signup.bind(this);
+    }
+
+    login(e) {
+        e.preventDefault();
+        var miusuario = this.usuario.current.value;
+        var micontraseña = this.contraseña.current.value;
+
+        firebase
+            .auth()
+            .createUserwithEmailAndPassword(miusuario, micontraseña)
+            .then(u => {})
+
+    }
+
+};
+}
+
+}
