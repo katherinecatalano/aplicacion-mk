02c4f82c13963ebbb7f0d6eae6cb27ad4f728ce7
katherinecatalano <84155740+katherinecatalano@users.noreply.github.com>
2022-11-21 19:56:28 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import React, { Component } from 'react';
-import Login from './UI/components/Formulario';
-import Inicio from './UI/components/Inicio';
-import { FirebaseApp } from 'firebase/app';
+import React from 'react'
 
-class App extends Component {
-
-    state = {
-        user: {}
-    }
-    constructor(props) {
-        super(props);
-    }
-    componentDidMount() {
-        this.authListener();
-    }
-    authListener() {
-        /*se comprueba si el usuario se logeo*/
-        FirebaseApp.auth().onAuthStateChanged((user) => {
-            if (user) {
-                /** usuario logeado*/
-                this.setState({ user });
-            } else {
-                /**usuario no logeado */
-                this.setState({ user: null });
-            }
-        });
-
-    }
-    render() {
-        return ( <
-            div className = 'App' > { this.state.user ? ( < home / > ) : ( < Login / > ) } <
-            /div>
-        );
-    }
+function App() {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+      
+    </div>
+  )
 }
+
+export default App;
