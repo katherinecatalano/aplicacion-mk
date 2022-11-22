478bedb4a8f57080d52f64ea665067a2778c3a71
katherinecatalano <84155740+katherinecatalano@users.noreply.github.com>
2022-11-22 14:09:54 -0300

    registrarse

--- src/UI/components/Registrarse.js
+++ src/UI/components/Registrarse.js
@@ -1,14 +1,31 @@
+import { useState } from "react";
 
 export function Registrarse (){
+
+const [user, setUser] = useState({
+  email:'',
+  password:'',
+});
+
+
+ const handleChange = (e) => {
+  console.log(e.target.name, e.target.value);
+ }
 
   return(
 
     <form>
       <label htmlFor="email">Email:</label>
-      <input type="email" placeholder="youremail@company.ltd"/>
+      <input type="email" name="email" placeholder="youremail@company.ltd"
+      onChange={handleChange}
+      />
 
       <label htmlFor="password">Password</label>
-      <input type="password" name="password" id="password" />
+      <input type="password" name="password" id="password|" 
+       onChange={handleChange}
+      />
+
+      <button>Registrarse</button>
 
     </form>
 
