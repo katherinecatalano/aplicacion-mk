a4000f9ab118890e1389fd80ab35f9e7348d6f68
katherinecatalano <84155740+katherinecatalano@users.noreply.github.com>
2022-11-21 21:46:46 -0300

    guardar datos

--- src/UI/components/Login.js
+++ src/UI/components/Login.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import { useState } from 'react';
 
  export function Login() {
+  
+  const{user,setUser}=useState({
+    email:'',
+    password: ''
+  })
+
+
   return (
-    <div>Login
+    <div>
+      <form>
+        <input type="email" name="email" id="email" />
+        <input type="password" name="password" id="email"/>
+
+      </form>
       
     </div>
   )
---
+++ src/context/Authcontext.js
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+
+const context = createContext();
+
+export function AuthProvider({children}){
+
+    /**todos los objetos hijos van a aparecer en const */
+    const user = {
+        login: true
+    }
+
+    return(
+        /** El provider sirve para despues exportar los componentes*/
+        <context.Provider value={{user}}>
+            {children}
+    
+        </context.Provider>
+    )
+}
